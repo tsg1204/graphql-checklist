d8ad7e84b6b5b37ec242ad7868b3f1866421c375
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-23 15:39:09 -0500

    useQuery tested

--- graphql-checklist/src/App.js
+++ graphql-checklist/src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_TODOS = gql`
+     query getTodos {
+       todos {
+         done
+         id
+         text
+       }
+   }`
+
 
 function App() {
+  const { data, loading, error } = useQuery(GET_TODOS);
+
+  if (loading) return <div>Loading todos ...</div>;
+  if (error) return <div>Error fetching todos.</div>
+
   return (
     <div >
-      App
+      {data.todos.map(todo => (
+        <p key={todo.id}>
+          <span>{todo.text}</span>
+          <button>&times;</button>
+        </p>
+      ))}
     </div>
   );
 }
--- graphql-checklist/src/index.js
+++ graphql-checklist/src/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'https://tsg-todo-graphql2.herokuapp.com/v1/graphql',
   cache: new InMemoryCache()
 })
 
-client.query({
-  query: gql`
-    query getTodos {
-      todos {
-        done
-        id
-        text
-      }
-  }`
-}).then(data => console.log(data))
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, 
+  document.getElementById('root')
+);
 
-ReactDOM.render(<App />,document.getElementById('root'));
-
+// client.query({
+//   query: gql`
+//     query getTodos {
+//       todos {
+//         done
+//         id
+//         text
+//       }
+//   }`
+// }).then(data => console.log(data))
