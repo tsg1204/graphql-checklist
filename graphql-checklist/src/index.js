e283670772250036d332f5346a654dd5fa136a42
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-23 13:28:22 -0500

    add ApolloClient to test

--- graphql-checklist/src/index.js
+++ graphql-checklist/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const client = new ApolloClient({
+  uri: 'https://tsg-todo-graphql2.herokuapp.com/v1/graphql',
+  cache: new InMemoryCache()
+})
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+client.query({
+  query: gql`
+    query getTodos {
+      todos {
+        done
+        id
+        text
+      }
+  }`
+}).then(data => console.log(data))
+
+ReactDOM.render(<App />,document.getElementById('root'));
+
