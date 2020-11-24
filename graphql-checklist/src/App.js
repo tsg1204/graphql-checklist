476c55f826dce2bdbae92537155d5d2d77b83d27
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-24 09:35:53 -0500

    add mutations

--- graphql-checklist/src/App.js
+++ graphql-checklist/src/App.js
@@ -21,13 +21,33 @@ const TOGGLE_TODO = gql`
     }
   }
 `
+const ADD_TODO = gql`
+  mutation addTodo($text: String!) {
+    insert_todos(objects: {text: $text}) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`
 
 function App() {
+  const [todoText, setTodoText] = React.useState('');
   const { data, loading, error } = useQuery(GET_TODOS);
   const [toggleTodo] = useMutation(TOGGLE_TODO);
+  const [addTodo] = useMutation(ADD_TODO);
 
   async function handleToggleTodo(todo) {
     await toggleTodo({ variables: { id: todo.id, done: !todo.done }})
+  }
+
+  async function handleAddTodo(event) {
+    event.preventDefault();
+    if (!todoText.trim()) return;
+    await addTodo({ variables: {text: todoText }});
+    setTodoText('');
   }
 
   if (loading) return <div>Loading todos ...</div>;
@@ -37,11 +57,13 @@ function App() {
     <div className="vh-100 code flex flex-column items-center bg-light-purple white pa3 fl-1">
       <h1 className="f2-1">GraphQL Checklist <span role="img" aria-label="Checkmark">✅</span></h1>
       {/* Todo Form */}
-      <form className="mb5">
+      <form onSubmit={handleAddTodo} className="mb3">
         <input 
           className="pa2 f4"
           type="text"
           placeholder="Write your todo"
+          onChange={event => setTodoText(event.target.value)}
+          value={todoText}
         />
         <button className="pa2 f4 bg-green" type="submit">Create</button>
       </form>
